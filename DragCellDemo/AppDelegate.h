8355e9a35b239b8da430fa9841cc08f3fb5cb559
zhumingke <ifeiteng007@sina.com>
2016-04-25 14:17:34 +0800

    Initial Commit

---
+++ DragCellDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DragCellDemo
+//
+//  Created by 朱明科 on 16/4/25.
+//  Copyright © 2016年 zhumingke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DragCellDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DragCellDemo
+//
+//  Created by 朱明科 on 16/4/25.
+//  Copyright © 2016年 zhumingke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
